075263e1cd1604e3b1820f4ca9517f5d5eb56abb
RamonWSU <ramonzazueta@mail.weber.edu>
2024-03-26 23:42:57 -0600

    Assignment 8

---
+++ src/javascripts/images.js
@@ -0,0 +1,73 @@
+export const images = [{
+    "id": 1,
+    "title": "Scuba In Reef",
+    "url": "https://images.unsplash.com/photo-1682687982423-295485af248a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "description": "Scuba Diver taking a photo of the reef"
+},
+{
+    "id": 2,
+    "title": "Earth from Satelite",
+    "url": "https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "description": "Earth from a satelites point of view"
+},
+{
+    "id": 3,
+    "title": "Clear blue sea",
+    "url": "https://images.unsplash.com/photo-1468581264429-2548ef9eb732?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "description": "Picture of a clear blue sea"
+},
+{
+    "id": 4,
+    "title": "Snow mountain under the stars",
+    "url": "https://images.unsplash.com/photo-1519681393784-d120267933ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "description": "A snowy mountain during a starry night"
+},
+{
+    "id": 5,
+    "title": "Little Fishes Coral",
+    "url": "https://plus.unsplash.com/premium_photo-1661841439995-1706237c83dc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "description": "Coral reef with lots of fishes"
+},
+{
+    "id": 6,
+    "title": "Mountain Alps",
+    "url": "https://images.unsplash.com/photo-1458668383970-8ddd3927deed?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "description": "A photo of the peak of the Alps covered in clouds"
+},
+{
+    "id": 7,
+    "title": "Fish in a Reef",
+    "url": "https://images.unsplash.com/photo-1583212292454-1fe6229603b7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "description": "A picture of fishes swimming in a coral reef"
+},
+{
+    "id": 8,
+    "title": "Neon Jellyfish",
+    "url": "https://images.unsplash.com/photo-1525467187333-8e3f68b8e742?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "description": "Three Neon jellyfish deep underwater"
+},
+{
+    "id": 9,
+    "title": "Bridge in the Mountains",
+    "url": "https://plus.unsplash.com/premium_photo-1669868116752-acd4bc7bc6a2?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "description": "A bridge connecting to a mountain"
+},
+{
+    "id": 10,
+    "title": "Sunrise Fields",
+    "url": "https://images.unsplash.com/photo-1500382017468-9049fed747ef?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "description": "A sunrise on a green field of land"
+},
+{
+    "id": 11,
+    "title": "Rocky Island",
+    "url": "https://images.unsplash.com/photo-1502085671122-2d218cd434e6?q=80&w=1226&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "description": "A very rocky island located in Ireland, the island is called Skellig Micheal"
+},
+{
+    "id": 12,
+    "title": "Lonely Desert",
+    "url": "https://images.unsplash.com/photo-1707345512638-997d31a10eaa?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "description": "A man alone in a vast desert "
+}
+]
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,32 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+
+
+import { images } from './images'
+
+for (let i of images) {
+    let i_thumb = document.getElementById('i' + i.id)
+    i_thumb.innerHTML = `
+    <img src="${i.url}" alt="${i.title}" class="img-thumbnail"/> 
+    `
+
+    i_thumb.onclick = function() {
+        displayImage(i)
+    }
+}
+
+let featured_image = document.querySelector(".featured")
+function displayImage(images){
+    featured_image.innerHTML = `
+    <div class="card">
+    <img src="${images.url}" class="card-img-top" alt="${images.title}">
+    <div class="card-body">
+    <h1 class="text-start">${images.title}</h1>
+    <p class="text-start">${images.description}</p>
+    </div>
+  </div>
+    `
+}
+
+
+
